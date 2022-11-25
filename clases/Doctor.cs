75c29f9aa55e1346538081e9c40668e92ceb9f94
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2022-11-24 23:42:20 -0600

    Actualizacion Miguel Clases

---
+++ clases/Doctor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoHospitalesGrupoLos4.clases
+{
+    internal class Doctor
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+        public string especialidad { get; set; }
+        public string gradoAcademico { get; set; }
+        public string estadoCivil { get; set; }
+        public int idHospital { get; set; }
+
+        public Doctor() { }
+        public Doctor(string nombre, string apellido, string telefono, string correo, string especialidad, string gradoAcademico, string estadoCivil, int idHospital)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.telefono = telefono;
+            this.correo = correo;
+            this.especialidad = especialidad;
+            this.gradoAcademico = gradoAcademico;
+            this.estadoCivil = estadoCivil;
+            this.idHospital = idHospital;
+        }
+        public static int agregarDoctor(Doctor doctor)
+        {
+            using (SqlConnection conexion = conexionBDJ.abrirConexion())
+            {
+                string query = "insert into Doctor (nombre, apellido, telefono,correo,especialidad,gradoAcademico,estadoCivil,idHospital) values('" + doctor.nombre + "', '" + doctor.apellido + "', '" + doctor.telefono + "', '" + doctor.correo + "','" + doctor.especialidad + "', '" + doctor.gradoAcademico + "','" + doctor.estadoCivil + "', '" + doctor.idHospital + "')";
+                SqlCommand comando = new SqlCommand(query, conexion);
+                comando.ExecuteNonQuery();
+            }
+            return 1;
+        }
+    }
+}
