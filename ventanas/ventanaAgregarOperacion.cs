1aa771f53a12e856e7f1a2916eb294c2a25e8e5b
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2022-11-25 22:59:12 -0600

    Actualizacion Miguel

--- ventanas/ventanaAgregarOperacion.cs
+++ ventanas/ventanaAgregarOperacion.cs
@@ -29,9 +29,9 @@ namespace proyectoHospitalesGrupoLos4.ventanas
         private void btnAgregar_Click(object sender, EventArgs e)
         {
             Operacion operacion = new Operacion();
-            operacion.idHospital = Convert.ToInt32(combo_IdHospital.Text.Split('-')[0]);
-            operacion.idPaciente = Convert.ToInt32(combo_IdPaciente.Text.Split('-')[0]);
-            operacion.idDoctor = Convert.ToInt32(combo_IdDoctor.Text.Split('-')[0]);
+            operacion.idHospital = Convert.ToInt32(combo_IdHospital.Text.Split(' ')[0]);
+            operacion.idPaciente = Convert.ToInt32(combo_IdPaciente.Text.Split(' ')[0]);
+            operacion.idDoctor = Convert.ToInt32(combo_IdDoctor.Text.Split(' ')[0]);
             operacion.fechaContrato = input_FechaContrato.Text;
             if (combo_TipoCirugia.Text == "")
             {
@@ -46,8 +46,22 @@ namespace proyectoHospitalesGrupoLos4.ventanas
             }
             Contrato contrato = new Contrato();
             contrato.idHospital = Convert.ToInt32(combo_IdHospital.Text);
-            contrato.idPaciente = Convert.ToInt32(combo_IdPaciente.Text.Split('-')[0]);
+            contrato.idPaciente = Convert.ToInt32(combo_IdPaciente.Text.Split(' ')[0]);
+            contrato.valorContrato = Convert.ToDouble(txt_ValorContrato.Text);
+            contrato.fechaContrato = input_FechaContrato.Text;
+            contrato.valorRestante = Convert.ToDouble(txt_ValorContrato.Text);
             contrato.codigoContrato = "CIR" + conexionBD.generadorDeNumerosRandoms();
+            contrato.nombre = combo_IdPaciente.Text.Split(' ')[1];
+            contrato.apellido = combo_IdPaciente.Text.Split(' ')[2];
+            if (combo_IdHospital.Text != "" || combo_IdDoctor.Text != "" || combo_IdPaciente.Text != "" || txt_ValorContrato.Text != "")
+            {
+                Contrato.agregarContrato(contrato);
+                Paciente.cambiarPacienteActivo(contrato.idPaciente);
+            }
+            else
+            {
+                MessageBox.Show("Faltan datos por digitar");
+            }
 
 
 
@@ -63,7 +77,7 @@ namespace proyectoHospitalesGrupoLos4.ventanas
         {
             SqlDataReader IdHospitalLector = conexionBD.traerInformacionDB("id", "Hospital", null, null);
             SqlDataReader IdPacienteLector = conexionBD.traerInformacionDB("id,nombre,apellido", "Paciente", null, null);
-            SqlDataReader IdDoctorLector = conexionBD.traerInformacionDB("id,nombre,apellido", "Doctor", null, null);
+            SqlDataReader doctorRender = conexionBD.traerInformacionDBDobleFiltro("nombre, id, apellido", "Doctor", "especialidad", "Cirujano", "idHospital", combo_IdHospital.Text);
             SqlDataReader idCirugiaLector = conexionBD.traerInformacionDB("nombre", null, null, null);
             while (IdHospitalLector.Read())
             {
@@ -71,11 +85,11 @@ namespace proyectoHospitalesGrupoLos4.ventanas
             }
             while (IdPacienteLector.Read())
             {
-                combo_IdPaciente.Items.Add(IdPacienteLector["id"].ToString() + "-" + IdPacienteLector["nombre"] + " " + IdPacienteLector["apellido"]);
+                combo_IdPaciente.Items.Add(IdPacienteLector["id"].ToString() + " " + IdPacienteLector["nombre"] + " " + IdPacienteLector["apellido"]);
             }
-            while (IdDoctorLector.Read())
+            while (doctorRender.Read())
             {
-                combo_IdDoctor.Items.Add(IdDoctorLector["id"].ToString() + "-" + IdDoctorLector["nombre"] + " " + IdDoctorLector["apellido"]);
+                combo_IdDoctor.Items.Add(doctorRender["id"].ToString() + " " +doctorRender["nombre"] + " " + doctorRender["apellido"]);
             }
             while (idCirugiaLector.Read())
             {
